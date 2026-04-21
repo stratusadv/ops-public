3b5c568e005ff84446fb638cb1379af7c5f9450c
Nathan Johnson <nathanj@stratusadv.com>
2026-04-20 19:54:40 -0600

    qwen asr

--- debian/vllm/install-vllm.sh
+++ debian/vllm/install-vllm.sh
@@ -54,6 +54,8 @@ source /root/vllm-venv/bin/activate
 
 uv pip install -U vllm --torch-backend=auto --extra-index-url https://wheels.vllm.ai/nightly
 uv pip install nvidia-cuda-runtime-cu12
+uv pip install qwen-asr[vllm]
+uv pip install vllm[audio]
 
 deactivate
 
