1d26216626a55ff9a026f4d91f1cefe610b47275
Nathan Johnson <nathanj@stratusadv.com>
2026-04-24 22:19:44 -0600

    update configuration

--- debian/vllm/vllm.sh
+++ debian/vllm/vllm.sh
@@ -2,10 +2,12 @@
 
 export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
 export VLLM_SKIP_P2P_CHECK=1
-export NCCL_P2P_DISABLE=1
+#export NCCL_P2P_DISABLE=1 # this has larger performance problems
+export NCCL_CUMEM_ENABLE=0
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
 source /root/vllm-venv/bin/activate
 
-vllm serve --config /root/vllm-config.yml --trust-remote-code --disable-custom-all-reduce
+vllm serve --config /root/vllm-config.yml --trust-remote-code --disable-custom-all-reduce --enable-prefix-caching
 
 deactivate
