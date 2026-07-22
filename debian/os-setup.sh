4ca7fce6d5243be518871abed394f51f29855a84
Nathan Johnson <nathanj@stratusadv.com>
2026-07-22 16:43:19 -0600

    os requirement fix

--- debian/os-setup.sh
+++ debian/os-setup.sh
@@ -8,5 +8,9 @@ apt install g++ freeglut3-dev build-essential libx11-dev libxmu-dev python3-dev 
 
 wget -qO- https://astral.sh/uv/install.sh | sh
 
+wget https://developer.download.nvidia.com/compute/cuda/13.1.0/local_installers/cuda_13.1.0_590.44.01_linux.run
 
+chmod +x cuda_13.1.0_590.44.01_linux.run
+
+./cuda_13.1.0_590.44.01_linux.run
 
