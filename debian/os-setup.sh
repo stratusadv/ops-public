baea4b403c29b6f3b5585a613b6b26341b4e895c
Nathan Johnson <nathanj@stratusadv.com>
2026-07-23 08:18:01 -0600

    os setup update

--- debian/os-setup.sh
+++ debian/os-setup.sh
@@ -4,11 +4,47 @@ apt update
 
 apt upgrade -y
 
-apt install g++ freeglut3-dev build-essential libx11-dev libxmu-dev python3-dev libxi-dev libglu1-mesa-dev libfreeimage-dev libglfw3-dev wget htop btop nvtop nano glances git cmake curl libcurl4-openssl-dev dkms proxmox-headers-$(uname -r)
+apt install -y \
+    btop \
+    build-essential \
+    cmake \
+    curl \
+    dkms \
+    freeglut3-dev \
+    g++ \
+    git \
+    glances \
+    htop \
+    libcurl4-openssl-dev \
+    libfreeimage-dev \
+    libglfw3-dev \
+    libglu1-mesa-dev \
+    libx11-dev \
+    libxi-dev \
+    libxmu-dev \
+    nano \
+    nvtop \
+    proxmox-headers-$(uname -r) \
+    python3-dev \
+    wget
 
 wget -qO- https://astral.sh/uv/install.sh | sh
 
-wget https://developer.download.nvidia.com/compute/cuda/13.3.1/local_installers/cuda_13.3.1_610.43.02_linux.run
+INSTALLER="cuda_13.3.1_610.43.02_linux.run"
 
-bash cuda_13.3.1_610.43.02_linux.run
+if command -v nvidia-smi &> /dev/null; then
+    echo "NVIDIA driver is already installed. Skipping CUDA download and installation. run 'nvidia-uninstall' to remove it"
+else
+    echo "NVIDIA driver not found. Preparing to install..."
+
+    if [ ! -f "$INSTALLER" ]; then
+        echo "Downloading CUDA installer..."
+        wget "https://developer.download.nvidia.com/compute/cuda/13.3.1/local_installers/$INSTALLER"
+    else
+        echo "Installer $INSTALLER already exists locally. Skipping download."
+    fi
+
+    echo "Running CUDA installer..."
+    bash "$INSTALLER" --silent
+fi
 
