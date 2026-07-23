2f770fb186cbdb3f6c19ba2e1030426dd17816ad
Nathan Johnson <nathanj@stratusadv.com>
2026-07-22 20:56:30 -0600

    os setup update

--- debian/os-setup.sh
+++ debian/os-setup.sh
@@ -4,7 +4,7 @@ apt update
 
 apt upgrade -y
 
-apt install g++ freeglut3-dev build-essential libx11-dev libxmu-dev python3-dev libxi-dev libglu1-mesa-dev libfreeimage-dev libglfw3-dev wget htop btop nvtop nano glances git cmake curl libcurl4-openssl-dev
+apt install g++ freeglut3-dev build-essential libx11-dev libxmu-dev python3-dev libxi-dev libglu1-mesa-dev libfreeimage-dev libglfw3-dev wget htop btop nvtop nano glances git cmake curl libcurl4-openssl-dev dkms proxmox-headers-$(uname -r)
 
 wget -qO- https://astral.sh/uv/install.sh | sh
 
