1ab2b8b899fa04354926ff0c6a9a9f4b28d9b53e
Nathan Johnson <nathanj@stratusadv.com>
2026-07-22 20:09:18 -0600

    vllm run script adjustment

--- debian/vllm/vllm.sh
+++ debian/vllm/vllm.sh
@@ -7,7 +7,7 @@ export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 #export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export NCCL_DEBUG=TRACE
-export CUDA_LAUNCH_BLOCKING=1
+#export CUDA_LAUNCH_BLOCKING=1
 export VLLM_LOGGING_LEVEL=DEBUG
 
 source /root/vllm-venv/bin/activate
