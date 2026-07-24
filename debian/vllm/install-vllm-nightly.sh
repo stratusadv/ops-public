5327fecbc64231fb421719d770eb3d134a709037
Nathan Johnson <nathanj@stratusadv.com>
2026-07-24 09:00:44 -0600

    vllm install update

--- debian/vllm/install-vllm-nightly.sh
+++ debian/vllm/install-vllm-nightly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./sh/environment-setup.sh &&
-./sh/vllm-nightly-install.sh &&
-./sh/vllm-service-install.sh
+source ./sh/environment-setup.sh &&
+source ./sh/vllm-nightly-install.sh &&
+source ./sh/vllm-service-install.sh
 
 echo "Nightly Vllm Install ✅ Done"
--- debian/vllm/install-vllm-stable.sh
+++ debian/vllm/install-vllm-stable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./sh/environment-setup.sh &&
-./sh/vllm-stable-install.sh &&
-./sh/vllm-service-install.sh
+source ./sh/environment-setup.sh &&
+source ./sh/vllm-stable-install.sh &&
+source ./sh/vllm-service-install.sh
 
 echo "Stable Vllm Install ✅ Done"
