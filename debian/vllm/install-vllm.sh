ad8fc62f7b7ffdfc7911cadb2a19e4bbaddec257
Nathan Johnson <nathanj@stratusadv.com>
2026-07-23 22:34:49 -0600

    vllm install update

--- debian/vllm/install-vllm.sh
+++ debian/vllm/install-vllm.sh
@@ -54,8 +54,8 @@ source /root/vllm-venv/bin/activate
 
 uv pip install qwen-asr[vllm]
 uv pip install vllm[audio]
-uv pip install nvidia-cuda-runtime-cu12
-uv pip install flashinfer-python --torch-backend=cu130
+#uv pip install nvidia-cuda-runtime-cu12
+#uv pip install flashinfer-python --torch-backend=cu130
 uv pip install -U vllm --pre \
   --extra-index-url https://wheels.vllm.ai/nightly/cu130 \
   --extra-index-url https://download.pytorch.org/whl/cu130 \
