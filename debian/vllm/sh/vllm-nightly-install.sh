8eb1698eb3fba21c3802cc8d46f8dea3156a964f
Nathan Johnson <nathanj@stratusadv.com>
2026-07-28 16:49:40 -0600

    qwen turbo update

--- debian/vllm/sh/vllm-nightly-install.sh
+++ debian/vllm/sh/vllm-nightly-install.sh
@@ -4,6 +4,8 @@ uv venv /root/vllm-venv
 
 source /root/vllm-venv/bin/activate
 
+bash <(curl -fsSL https://raw.githubusercontent.com/vllm-project/vllm/main/tools/install_deepgemm.sh) --cuda-version 13.3
+
 uv pip install qwen-asr[vllm]
 uv pip install vllm[audio]
 uv pip install nvidia-cuda-runtime-cu12
