caca8c81a684ce1340d6920cb93089ccc1d2ee43
Nathan Johnson <nathanj@stratusadv.com>
2026-07-28 17:00:40 -0600

    vllm run config updated

--- debian/vllm/sh/vllm-stable-install.sh
+++ debian/vllm/sh/vllm-stable-install.sh
@@ -4,8 +4,6 @@ uv venv /root/vllm-venv --python 3.12
 
 source /root/vllm-venv/bin/activate
 
-bash <(curl -fsSL https://raw.githubusercontent.com/vllm-project/vllm/main/tools/install_deepgemm.sh) --cuda-version 13.3
-
 uv pip install qwen-asr[vllm]
 uv pip install vllm[audio]
 uv pip install -U vllm
