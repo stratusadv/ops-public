b39996e3089de7c07df91997c4727e8a59a85bd4
Nathan Johnson <nathanj@stratusadv.com>
2026-07-29 09:55:52 -0600

    remote development

--- debian/development/setup-remote-development.sh
+++ debian/development/setup-remote-development.sh
@@ -9,7 +9,6 @@ apt install -y \
     curl \
     git \
     gh \
-    helix \
     lazygit \
     python3 \
     python3-dev \
@@ -19,11 +18,9 @@ apt install -y \
 
 echo "Updating Repositories"
 
-curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/debian.griffo.io.gpgecho "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main"
-echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | sudo tee /etc/apt/sources.list.d/debian.griffo.io.list
-
-apt update
-apt install helix
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix
 
 echo "Installing UV"
 
@@ -31,7 +28,7 @@ wget -qO- https://astral.sh/uv/install.sh | sh
 
 source ~/.bashrc
 
-uvx install ruff
-uvx install ty
-uvx install python-lsp-server
+uv tool install ruff
+uv tool install ty
+uv tool install python-lsp-server
 
