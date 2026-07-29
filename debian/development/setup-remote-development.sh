a0df7c0bde7739ac1577642046c78f98eaeeeab2
Nathan Johnson <nathanj@stratusadv.com>
2026-07-29 10:07:24 -0600

    remote development

--- debian/development/setup-remote-development.sh
+++ debian/development/setup-remote-development.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo "Installing Packages"
+echo "Setting up Remote Development"
 
 apt update
 apt upgrade -y
 
 apt install -y \
+    build-essential \
     curl \
     git \
     gh \
     hx \
     lazygit \
+    libpq-dev \
     python3 \
     python3-dev \
     python3-pip \
     python3-venv \
     wget
-
-echo "Installing UV"
 
 wget -qO- https://astral.sh/uv/install.sh | sh
 
@@ -27,3 +27,8 @@ uv tool install ruff
 uv tool install ty
 uv tool install python-lsp-server
 
+hx --grammar fetch
+hx --grammar build
+hx --health python
+
+echo "✅ Done"
