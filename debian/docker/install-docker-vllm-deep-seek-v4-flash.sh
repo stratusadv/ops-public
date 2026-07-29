47a39a64590493be4b6354017012647971b37914
Nathan Johnson <nathanj@stratusadv.com>
2026-07-28 20:11:10 -0600

    deep seek docker

---
+++ debian/docker/install-docker-vllm-deep-seek-v4-flash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker pull vllm/vllm-openai:latest
+
+docker run --gpus all \
+  --privileged --ipc=host -p 8000:8000 \
+  -v ~/.cache/huggingface:/root/.cache/huggingface \
+  vllm/vllm-openai:latest nvidia/DeepSeek-V4-Flash-NVFP4 \
+  --trust-remote-code \
+  --kv-cache-dtype fp8 \
+  --block-size 256 \
+  --enable-expert-parallel \
+  --tensor-parallel-size 2 \
+  --attention_config.use_fp4_indexer_cache=True \
+  --max-model-len 262,144 \
+  --tokenizer-mode deepseek_v4 \
+  --tool-call-parser deepseek_v4 \
+  --enable-auto-tool-choice \
+  --reasoning-parser deepseek_v4\
+  --default-chat-template-kwargs '{ "enable_thinking": true, "reasoning_effort": "max" }'
