a4081886da99adba2b00bfe036cf431f9c24e58c
Nathan Johnson <nathanj@stratusadv.com>
2026-08-02 21:35:44 -0600

    docker configuration

--- debian/docker/install-docker-vllm-deep-seek-v4-flash-0731.sh
+++ debian/docker/install-docker-vllm-deep-seek-v4-flash-0731.sh
@@ -7,9 +7,9 @@ docker run -d --restart unless-stopped --gpus all \
   --label autoheal=true \
   --privileged --ipc=host -p 8000:8000 \
   --health-cmd="curl -f http://localhost:8000/health || exit 1" \
-  --health-interval=10s \
+  --health-interval=15s \
   --health-timeout=5s \
-  --health-retries=5 \
+  --health-retries=3 \
   --health-start-period=600s \
   -v ~/.cache/huggingface:/root/.cache/huggingface \
   vllm/vllm-openai:latest auroter/DeepSeek-V4-Flash-0731-NVFP4 \
