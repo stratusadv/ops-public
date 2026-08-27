ef0d3cc69d286bf139cc5030f9b362f2deefb4b6
Nathan Johnson <nathanj@stratusadv.com>
2026-08-27 13:55:49 -0600

    deepseek flash config

--- debian/docker/setup-docker-vllm-deep-seek-v4-flash-0731.sh
+++ debian/docker/setup-docker-vllm-deep-seek-v4-flash-0731.sh
@@ -16,17 +16,23 @@ docker run -d --restart unless-stopped --gpus all \
   -v ~/.cache/huggingface:/root/.cache/huggingface \
   vllm/vllm-openai:latest auroter/DeepSeek-V4-Flash-0731-NVFP4 \
   --served-model-name 'stratus.thinking' \
-  --trust-remote-code \
-  --kv-cache-dtype fp8 \
-  --gpu-memory-utilization 0.95 \
   --block-size 256 \
-  --tensor-parallel-size 2 \
+  --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}' \
+  --cpu-offload-gb 32 \
+  --enable-auto-tool-choice \
+  --enable-expert-parallel \
+  --enable-prefix-caching \
+  --gpu-memory-utilization 0.95 \
+  --kv-cache-dtype fp8 \
   --max-model-len 262144 \
   --max-num-seqs 2 \
+  --moe-backend auto \
+  --reasoning-config '{"reasoning_parser":"deepseek_v4","reasoning_start_str":"","reasoning_end_str":""}' \
+  --reasoning-parser deepseek_v4 \
+  --speculative-config '{"method":"dspark","num_speculative_tokens":5}' \
+  --tensor-parallel-size 2 \
   --tokenizer-mode deepseek_v4 \
   --tool-call-parser deepseek_v4 \
-  --enable-auto-tool-choice \
-  --reasoning-parser deepseek_v4 \
-  --compilation-config '{"cudagraph_mode":"FULL_DECODE_ONLY"}'
+  --trust-remote-code
 
 source ./sh/vllm-docker-restart-service-install.sh
