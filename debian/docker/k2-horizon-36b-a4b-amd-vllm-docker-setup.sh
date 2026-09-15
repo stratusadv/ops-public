51359d1dbd2a0c18c7ea8de5f29aff84d2b8d217
Nathan Johnson <nathanj@stratusadv.com>
2026-09-15 14:40:08 -0600

    new vllm config

---
+++ debian/docker/k2-horizon-36b-a4b-amd-vllm-docker-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ./sh/vllm-docker-stop-and-remove.sh
+
+docker pull vllm/vllm-openai:latest
+
+docker run -d --restart unless-stopped --gpus all \
+  --name vllm \
+  --label autoheal=true \
+  --privileged --ipc=host -p 8000:8000 \
+  --health-cmd='curl -f http://localhost:8000/health || exit 1' \
+  --health-interval=15s \
+  --health-timeout=5s \
+  --health-retries=3 \
+  --health-start-period=600s \
+  -v ~/.cache/huggingface:/root/.cache/huggingface \
+  vllm/vllm-openai-rocm:latest cyankiwi/K2-Horizon-MoVA-36B-A4B-AWQ-INT4 \
+  --served-model-name 'dandy.dash' \
+  --trust-remote-code \
+  --enable-expert-parallel \
+  --gpu-memory-utilization 0.90 \
+  --kv-cache-dtype fp8 \
+  --tensor-parallel-size 2 \
+  --reasoning-parser k2_horizon \
+  --enable-auto-tool-choice \
+  --tool-call-parser k2_horizon \
+  --enable-prefix-caching \
+  --default-chat-template-kwargs '{"reasoning_effort": "low"}'
+
+source ./sh/vllm-docker-restart-service-install.sh
